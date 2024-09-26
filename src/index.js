940875708bb0bf3875f15bbc652bc1131785ec48
Connor McElroy <connor.mcelroy@outlook.com>
2024-09-26 12:40:03 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-export { default as AuthenticationManager } from './screens/AuthenticationManager';
-export { default as SubscriptionManager } from './screens/SubscriptionManager';
-export { default as useAuthContext } from './hooks/useAuthContext';
-export { default as useApi } from './hooks/useApi';
-export { default as colors } from './themes/colors';
-export { default as DrawerContent } from './components/DrawerContent';
+export { default as AuthenticationManager } from './src/screens/AuthenticationManager';
+export { default as SubscriptionManager } from './src/screens/SubscriptionManager';
+export { default as useAuthContext } from './src/hooks/useAuthContext';
+export { default as useApi } from './src/hooks/useApi';
+export { default as colors } from './src/themes/colors';
+export { default as DrawerContent } from './src/components/DrawerContent';
